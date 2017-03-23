36cebc4c1dfe1fa0f8f201c0fd74eaf0c0812a17
Brandon Slaa <branslaa@gmail.com>
2017-03-22 21:33:15 -0400

    Created basic treeview and file pathing

---
+++ ExplorerProject/Folder.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.ComponentModel;
+
+namespace ExplorerProject
+{
+	public class Folder : INotifyPropertyChanged
+	{
+		public string Name { get; set; }
+		public string Path { get; set; }
+		public bool? IsChecked { get; set; }
+		public string picture { get; set; }
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		void NotifiyPropertyChanged(string property)
+		{
+			if (PropertyChanged != null)
+				PropertyChanged(this, new PropertyChangedEventArgs(property));
+		}
+	}
+
+	public class File : Folder
+	{
+		public int Size { get; set; }
+	}
+
+	public class DirectoryFolder : Folder
+	{
+		public List<Folder> Folders { get; set; }
+
+		public DirectoryFolder()
+		{
+			Folders = new List<Folder>();
+		}
+	}
+
+	public class FolderProvider
+	{
+		public List<Folder> GetFolders(string path)
+		{
+			var items = new List<Folder>();
+
+			var dirInfo = new DirectoryInfo(path);
+
+
+
+			foreach (var directory in dirInfo.GetDirectories())
+			{
+
+				var folder = new DirectoryFolder
+				{
+					Name = directory.Name,
+					Path = directory.FullName,
+					IsChecked = false,
+					picture = "folder.png",
+				Folders = GetFolders(directory.FullName)
+				};
+
+				items.Add(folder);
+			}
+
+			foreach (var file in dirInfo.GetFiles())
+			{
+				var item = new File
+				{
+					Name = file.Name,
+					Path = file.FullName,
+					IsChecked = false
+					
+				};
+
+				items.Add(item);
+			}
+
+
+
+			return items;
+		}
+	}
+}
---
+++ ExplorerProject/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExplorerProject
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			var folderProvider = new FolderProvider();
+
+			var folders = folderProvider.GetFolders(PathProperty);
+
+			this.Loaded += MainWindow_Loaded;
+			treeView.DataContext = folders;
+		}
+
+		public void MainWindow_Loaded(object sender, RoutedEventArgs e)
+		{
+			this.DataContext = this;
+		}
+
+
+
+		public string PathProperty
+		{
+			get { return (string)GetValue(PathPropertyProperty); }
+			set { SetValue(PathPropertyProperty, value); }
+		}
+
+		// Using a DependencyProperty as the backing store for PathProperty.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty PathPropertyProperty =
+			DependencyProperty.Register("PathProperty", typeof(string), typeof(MainWindow), new PropertyMetadata(AppDomain.CurrentDomain.BaseDirectory));
+
+	}
+}
